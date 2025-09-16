48b131e5c0aedb1c93ba038b515f13a8b030f64e
Sahin <sahin0945@gmail.com>
2025-09-17 02:01:43 +0530

    Major UI improvements

--- src/pages/AssessmentFormWrapper.jsx
+++ src/pages/AssessmentFormWrapper.jsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import AssessmentForm from "./AssessmentForm";
+import AssessmentForm from "../components/AssessmentForm";
 
 export default function AssessmentFormWrapper() {
   const navigate = useNavigate();
